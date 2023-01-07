8674991b1559e9f01bd0555c448d49161b5ca2a6
natigaminov <natig.aminov@gmail.com>
2023-01-07 23:42:52 +0300

    Add files via upload (fork ecoiot-web)

---
+++ src/waspmote/gases-pro/toggle.h
@@ -0,0 +1,9 @@
+/* 
+ * Для переключения модуля нужно поменять объявления макросов
+ */
+
+#if true // чтобы было MODEL_BLUE надо поставить false 
+  #define MODEL_GREEN
+#else
+  #define MODEL_BLUE
+#endif
